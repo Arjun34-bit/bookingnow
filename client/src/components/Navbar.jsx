5029745e40a85a45ff5e4e9cb2eeeedfbd89db59
Arjun Devendra <arjundevendra34@gmail.com>
2025-03-06 17:44:14 +0530

    BookNow.com

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -23,7 +23,7 @@ const Navbar = () => {
     <nav className="bg-blue-600 text-white p-4 shadow-md">
       <div className="container mx-auto flex justify-between items-center">
         <Link to="/" className="text-2xl font-bold">
-          BookingNow.com
+          BookNow.com
         </Link>
 
         {/* Desktop Menu */}
